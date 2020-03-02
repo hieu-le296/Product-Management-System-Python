8fecf29ad8362f18902f0788856c74b6db0b6100
Hieu.Le <hieu.le@student.ufv.ca>
2020-03-02 11:20:18 -0800

    Creating toolbar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
 
 class Main(QMainWindow):
     def __init__(self):
@@ -14,7 +15,26 @@ class Main(QMainWindow):
         self.show()
 
     def UI(self):
-        pass
+        self.toolBar()
+
+    def toolBar(self):
+        self.tb = self.addToolBar("Tool Bar")
+        self.tb.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        ############Toolbar Buttons##########
+        ############Add Product##############
+        self.addProduct = QAction(QIcon('icons/add.png'), "Add Product", self)
+        self.tb.addAction(self.addProduct)
+        self.tb.addSeparator()
+        ############Add Member##############
+        self.addMember = QAction(QIcon('icons/users.png'), "Add Memeber", self)
+        self.tb.addAction(self.addMember)
+        self.tb.addSeparator()
+        ############Selling##############
+        self.sellProduct = QAction(QIcon('icons/sell.png'), "Sell Product", self)
+        self.tb.addAction(self.sellProduct)
+        self.tb.addSeparator()
+
+
 
 
 def main():
