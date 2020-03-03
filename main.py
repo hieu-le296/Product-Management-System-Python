d23a46130f2fb69f67853c2b6ab67c27b401f954
Hieu.Le <hieu.le@student.ufv.ca>
2020-03-03 11:59:23 -0800

    Creating database

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
+import sqlite3
+
+sqlConnect = sqlite3.connect("products.db")
+cur = sqlConnect.cursor()
 
 class Main(QMainWindow):
     def __init__(self):
