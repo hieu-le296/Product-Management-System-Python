0f06b6c1efb8645dc97d816f19f1be3ebf21e11d
Hieu.Le <hieu.le@student.ufv.ca>
2020-03-03 16:07:40 -0800

    Creating layouts and widgets for Add Member window

--- add_member.py
+++ add_member.py
@@ -22,4 +22,52 @@ class AddMember(QWidget):
         self.show()
 
     def UI(self):
-        pass
+        self.widgets()
+        self.layouts()
+
+    def widgets(self):
+        ##############widgets of top layout################
+        self.addMemberImg = QLabel()
+        self.img = QPixmap('icons/addmember.png')
+        self.addMemberImg.setPixmap(self.img)
+        self.addMemberImg.setAlignment(Qt.AlignCenter)
+        self.titleText = QLabel("Add Member")
+        self.titleText.setAlignment(Qt.AlignCenter)
+
+        ##############widgets of bottom layout################
+        self.fnameEntry = QLineEdit()
+        self.fnameEntry.setPlaceholderText("Enter member first name")
+        self.lnameEntry = QLineEdit()
+        self.lnameEntry.setPlaceholderText("Enter member last name")
+        self.phoneEntry = QLineEdit()
+        self.phoneEntry.setPlaceholderText("Enter member phone")
+        self.addressEntry = QLineEdit()
+        self.addressEntry.setPlaceholderText("Enter member full address")
+        self.submitBtn = QPushButton("Submit")
+
+
+
+    def layouts(self):
+        self.mainLayout = QVBoxLayout()
+        self.topLayout = QVBoxLayout()
+        self.bottomLayout = QFormLayout()
+        self.topFrame = QFrame()
+        self.bottomFrame = QFrame()
+
+        ##############Add Widgets################
+        self.topLayout.addWidget(self.titleText)
+        self.topLayout.addWidget(self.addMemberImg)
+        self.topFrame.setLayout(self.topLayout)
+
+        self.bottomLayout.addRow(QLabel("First Name: "), self.fnameEntry)
+        self.bottomLayout.addRow(QLabel("Last Name: "), self.lnameEntry)
+        self.bottomLayout.addRow(QLabel("Phone Number: "), self.phoneEntry)
+        self.bottomLayout.addRow(QLabel("Full Address: "), self.addressEntry)
+        self.bottomLayout.addRow(QLabel(""), self.submitBtn)
+        self.bottomFrame.setLayout(self.bottomLayout)
+
+        self.mainLayout.addWidget(self.topFrame)
+        self.mainLayout.addWidget(self.bottomFrame)
+
+        self.setLayout(self.mainLayout)
+
