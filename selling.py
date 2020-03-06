864d451b69624122250cd8a6e55479cbdb169d09
Hieu.Le <hieu.le@student.ufv.ca>
2020-03-06 10:15:46 -0800

    Fetch Data from databse to sell window

--- selling.py
+++ selling.py
@@ -25,6 +25,7 @@ class SellProduct(QWidget):
     def UI(self):
         self.widgets()
         self.layouts()
+        self.fetchData()
 
     def widgets(self):
         ##############Widgets of Top Layout##############
@@ -36,9 +37,25 @@ class SellProduct(QWidget):
         self.titleText.setAlignment(Qt.AlignCenter)
         ##############Widgets of Bottom Layout##############
         self.productCombo = QComboBox()
+        self.productCombo.currentIndexChanged.connect(self.changeComboValue)
         self.memberCombo = QComboBox()
         self.quantityCombo = QComboBox()
-        self.submitBtn = QPushButton("Sumit")
+        self.submitBtn = QPushButton("Submit")
+
+        # query1 = "SELECT * FROM products WHERE product_availability =?"
+        # query2 = "SELECT member_id, member_fname from members"
+        # products = cur.execute(query1, ('Available',)).fetchall()
+        # members = cur.execute(query2).fetchall()
+        # quantity = products[0][4]
+        #
+        # for product in products:
+        #     self.productCombo.addItem(product[1], product[0])
+        #
+        # for member in members:
+        #     self.memberCombo.addItem(member[1], member[0])
+        #
+        # for i in range(1, quantity + 1):
+        #     self.quantityCombo.addItem(str(i))
 
 
     def layouts(self):
@@ -65,7 +82,33 @@ class SellProduct(QWidget):
         self.mainLayout.addWidget(self.bottomFrame)
         self.setLayout(self.mainLayout)
 
+    def fetchData(self):
+        query1 = "SELECT * FROM products WHERE product_availability =?"
+        query2 = "SELECT member_id, member_fname from members"
+        products = cur.execute(query1, ('Available',)).fetchall()
+        members = cur.execute(query2).fetchall()
+        quantity = products[0][4]
 
+
+        for product in products:
+            self.productCombo.addItem(product[1], product[0])
+
+        for member in members:
+            self.memberCombo.addItem(member[1], member[0])
+
+        self.quantityCombo.clear()
+
+        for i in range(1, quantity + 1):
+            self.quantityCombo.addItem(str(i))
+
+    def changeComboValue(self):
+        self.quantityCombo.clear()
+        product_id = self.productCombo.currentData()
+        query = ("SELECT product_quota FROM products WHERE product_id =?")
+        quota = cur.execute(query, (product_id,)).fetchone()
+
+        for i in range(1, quota[0] + 1):
+            self.quantityCombo.addItem(str(i))
 
 
 
