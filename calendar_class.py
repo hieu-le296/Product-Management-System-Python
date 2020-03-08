c71ec458bff27bab12214f0814bb9959a4dce1db
Hieu Le <hieu.le@student.ufv.ca>
2020-03-07 23:58:13 -0800

    Edit calendar window

--- calendar_class.py
+++ calendar_class.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import calendar
-from PyQt5.QtWidgets import QWidget, QCalendarWidget
-from PyQt5.QtCore import QDate
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 
 class Calendar(QWidget):
@@ -13,13 +13,16 @@ class Calendar(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Calendar')
-        self.setGeometry(800, 300, 350, 250)
-        self.setFixedSize(self.size())
+        self.setGeometry(800, 300, 350, 300)
         self.UI()
         self.show()
 
     def UI(self):
+        vbox = QVBoxLayout()
         self.calendar = QCalendarWidget(self)
+        vbox.addWidget(self.calendar)
+        self.setLayout(vbox)
+
         self.calendar.move(20, 20)
         self.calendar.setGridVisible(True)
 
