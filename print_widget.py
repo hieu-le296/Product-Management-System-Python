1ab3f73ab762e1c7df1d37b15befb803e9bf6702
Hieu Le <hieu.le@student.ufv.ca>
2020-03-09 03:04:50 -0700

    Fixing printer_widget class

--- print_widget.py
+++ print_widget.py
@@ -11,7 +11,7 @@ class Print(QWidget):
         super().__init__()
         self.setWindowTitle("Print Preview")
         self.setWindowIcon(QIcon('icons/icon.ico'))
-        self.setGeometry(850, 450, 250, 150)
+        self.setGeometry(850, 450, 370, 150)
         self.setFixedSize(self.size())
         self.UI()
         self.show()
@@ -24,8 +24,10 @@ class Print(QWidget):
         self.product.move(40, 50)
         self.member = QRadioButton("Membership", self)
         self.member.move(140, 50)
+        self.selling = QRadioButton('Selling History', self)
+        self.selling.move(240, 50)
         self.printBtn = QPushButton("Open Print Dialog", self)
-        self.printBtn.move(90, 100)
+        self.printBtn.move(150, 100)
         self.printBtn.clicked.connect(self.printPreviewDialog)
 
     def printPreviewDialog(self):
@@ -37,18 +39,65 @@ class Print(QWidget):
     def printPreview(self, printer):
         self.textEdit = QTextEdit(self)
         if self.product.isChecked():
-            query = cur.execute(
-                "SELECT product_id, product_name, product_manufacturer, product_price, product_quota, product_availability FROM products")
-            self.textEdit.append("                Product List        ")
-            self.textEdit.append("(product ID, product name, manufacturer, price, quota, availability)")
-            for row_data in query:
-                self.textEdit.append(str(row_data))
+            products = cur.execute(
+                "SELECT product_id, product_name, product_manufacturer, product_price, product_quota, product_date, product_availability FROM products")
+            self.textEdit.append(
+                "                                                                              Product List        ")
+            for product in products:
+                product_id = product[0]
+                product_name = product[1]
+                product_manu = product[2]
+                product_price = product[3]
+                product_quota = product[4]
+                product_date = product[5]
+                product_status = product[6]
+                self.textEdit.append("Product ID: " + str(product_id))
+                self.textEdit.append("Product Name: " + product_name)
+                self.textEdit.append("Manufacturer: " + product_manu)
+                self.textEdit.append("Price: $" + str(product_price))
+                self.textEdit.append("Quantity: " + str(product_quota))
+                self.textEdit.append("Date Added: " + str(product_date))
+                self.textEdit.append("Status: " + product_status)
+                self.textEdit.append("")
         elif self.member.isChecked():
-            query = cur.execute("SELECT * FROM members")
-            self.textEdit.append("                Membership List        ")
-            self.textEdit.append("(member id, first name, last name, phone number, address)")
-            for row_member in query:
-                self.textEdit.append(str(row_member))
+            checked = True
+            members = cur.execute("SELECT * FROM members")
+            self.textEdit.append(
+                "                                                                                  Membership List        ")
+            for member in members:
+                member_id = member[0]
+                member_fname = member[1]
+                member_lname = member[2]
+                member_phone = member[3]
+                member_addr = member[4]
+                self.textEdit.append("Member ID: " + str(member_id))
+                self.textEdit.append("First name: " + member_fname)
+                self.textEdit.append("Last name: " + str(member_lname))
+                self.textEdit.append("Phone number: " + str(member_phone))
+                self.textEdit.append("Address: " + str(member_addr))
+                self.textEdit.append("")
+
+        elif self.selling.isChecked():
+            checked = True
+            query = "SELECT products.product_name, products.product_manufacturer, product_price, members.member_fname, sellings.selling_quantity, sellings.selling_amount, sellings.selling_date FROM products, members, sellings WHERE products.product_id = sellings.selling_id AND members.member_id = sellings.selling_member_id"
+            history = cur.execute(query).fetchall()
+            self.textEdit.append("                                                                               Selling History        ")
+            for record in history:
+                product_name = record[0]
+                product_manu = record[1]
+                product_price = record[2]
+                member_name = record[3]
+                selling_quantity = record[4]
+                selling_amount = record[5]
+                selling_date = record[6]
+                self.textEdit.append("Product Name: " + product_name)
+                self.textEdit.append("Manufacturer: " + product_manu)
+                self.textEdit.append("Product Price: $" + str(product_price))
+                self.textEdit.append("Selling to: " + member_name)
+                self.textEdit.append("Quantity: " + str(selling_quantity))
+                self.textEdit.append("Amount: $" + str(selling_amount))
+                self.textEdit.append("Date Sold: " + str(selling_date))
+                self.textEdit.append("")
 
         self.textEdit.document().print_(printer)
 
