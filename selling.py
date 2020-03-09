9212f9c827d58f2a9ace1d8b34f69af7098c8e67
Hieu Le <hieu.le@student.ufv.ca>
2020-03-08 23:51:59 -0700

    Added date to selling database and printing receipt

--- selling.py
+++ selling.py
@@ -1,6 +1,8 @@
+from PyQt5 import QtPrintSupport, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
+from datetime import datetime
 import sqlite3
 import styles
 import main
@@ -9,6 +11,7 @@ sqlConnect = sqlite3.connect("products.db")
 cur = sqlConnect.cursor()
 
 defaultImg = 'img/store.png'
+
 
 class SellProduct(QWidget):
     def __init__(self):
@@ -28,7 +31,7 @@ class SellProduct(QWidget):
 
     def widgets(self):
         ##############Widgets of Top Layout##############
-        self.productImg = QLabel()
+        self.productImg = QLabel(self)
         self.img = QPixmap('icons/shop.png')
         self.productImg.setPixmap(self.img)
         self.productImg.setAlignment(Qt.AlignCenter)
@@ -76,7 +79,6 @@ class SellProduct(QWidget):
         members = cur.execute(query2).fetchall()
         quantity = products[0][4]
 
-
         for product in products:
             self.productCombo.addItem(product[1], product[0])
 
@@ -116,6 +118,7 @@ class SellProduct(QWidget):
         self.topFrame.setStyleSheet(styles.sellProductTopFrame())
         self.bottomFrame.setStyleSheet(styles.sellProductBottomFrame())
 
+
 class ConfirmWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -141,7 +144,7 @@ class ConfirmWindow(QWidget):
         self.titleText.setAlignment(Qt.AlignCenter)
 
         ##############widgets of bottom layout################
-        global productName, productId, memberName, memberId, quantity
+        global productName, productId, memberName, memberId, quantity, price
         priceQuery = "SELECT product_price FROM products WHERE product_id =?"
         price = cur.execute(priceQuery, (productId,)).fetchone()
         self.amount = quantity * price[0]
@@ -152,8 +155,20 @@ class ConfirmWindow(QWidget):
         self.memberName.setText(memberName)
         self.amountLabel = QLabel()
         self.amountLabel.setText(str(price[0]) + "x" + str(quantity) + " = $" + str(self.amount))
+        x = datetime.now()
+        self.dateString = '{}/{}/{}'.format(x.day, x.month, x.year)
+        self.dateSold = QLabel()
+        self.dateSold.setText(self.dateString)
         self.confirmBtn = QPushButton("Confirm")
         self.confirmBtn.clicked.connect(self.confirm)
+
+        self.textEdit = QTextEdit(self)
+        self.textEdit.setReadOnly(True)
+        self.textEdit.append("Selling Receipt")
+        self.textEdit.append("Product: " + productName)
+        self.textEdit.append("Customer Name: " + memberName)
+        self.textEdit.append("Quantity: " + str(quantity))
+        self.textEdit.append("Amount: $" + str(price[0]) + "x" + str(quantity) + " = $" + str(self.amount))
 
     def layouts(self):
         self.mainLayout = QVBoxLayout()
@@ -167,9 +182,7 @@ class ConfirmWindow(QWidget):
         self.topLayout.addWidget(self.productImg)
         self.topFrame.setLayout(self.topLayout)
 
-        self.bottomLayout.addRow(QLabel("Product: "), self.productName)
-        self.bottomLayout.addRow(QLabel("Selling to: "), self.memberName)
-        self.bottomLayout.addRow(QLabel("Amount: "), self.amountLabel)
+        self.bottomLayout.addWidget(self.textEdit)
         self.bottomLayout.addRow(QLabel(""), self.confirmBtn)
         self.bottomFrame.setLayout(self.bottomLayout)
 
@@ -180,10 +193,11 @@ class ConfirmWindow(QWidget):
 
     def confirm(self):
         global productName, productId, memberName, memberId, quantity
+
         try:
-            sellQuery = "INSERT INTO 'sellings' (selling_product_id, selling_member_id, selling_quantity, selling_amount) VALUES (?,?,?,?)"
+            sellQuery = "INSERT INTO 'sellings' (selling_product_id, selling_member_id, selling_quantity, selling_amount, selling_date) VALUES (?,?,?,?,?)"
             quotaQuery = ("SELECT product_quota FROM products WHERE product_id =?")
-            cur.execute(sellQuery, (productId, memberId, quantity, self.amount))
+            cur.execute(sellQuery, (productId, memberId, quantity, self.amount, self.dateString))
             self.quota = cur.execute(quotaQuery, (productId,)).fetchone()
             sqlConnect.commit()
 
@@ -196,17 +210,22 @@ class ConfirmWindow(QWidget):
                 updateQuotaQuery = "UPDATE products set product_quota =? WHERE product_id =?"
                 cur.execute(updateQuotaQuery, (newQuota, productId))
                 sqlConnect.commit()
-            QMessageBox.information(self, "Info", "Success")
+
+            mbox = QMessageBox.question(self, "Success", "Would you like to print a selling receipt?", QMessageBox.Yes | QMessageBox.No)
+            if mbox == QMessageBox.Yes:
+                self.printReceipt()
+
             self.close()
             self.main = main.Main()
             self.main.show()
         except:
             QMessageBox.information(self, "Info", "Something went wrong")
 
+    def printReceipt(self):
+        # Create printer
+        dialog = QtPrintSupport.QPrintDialog()
+        if dialog.exec_() == QtWidgets.QDialog.Accepted:
+            self.textEdit.document().print_(dialog.printer())
     def styles(self):
         self.topFrame.setStyleSheet(styles.confirmProductTopFrame())
         self.bottomFrame.setStyleSheet(styles.confirmProductBottomFrame())
-
-
-
-
